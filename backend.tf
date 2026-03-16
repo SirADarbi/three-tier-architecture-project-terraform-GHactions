79e05a40b1a62139d46eb439e24a28cea5dd9825
SIRAJ_THINKPAD\Siraj <sirajdarbi@gmail.com>
2026-03-16 12:34:00 -0600

    fixed backend, replaced deprecated dynamodb_table with use_lockfile

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "three-tier-architecture-terraform-state"
-    key            = "prod/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "three-tier-architecture-terraform-lock"
-    encrypt        = true
+    bucket       = "three-tier-architecture-terraform-state"
+    key          = "prod/terraform.tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+    encrypt      = true
   }
 }
