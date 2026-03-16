65d1b50df23fc5b408d3a2775158b8d7e4d37256
SIRAJ_THINKPAD\Siraj <sirajdarbi@gmail.com>
2026-03-16 10:31:24 -0600

    initial commit: three-tier aws architecture with Terraform

---
+++ modules/security/outputs.tf
@@ -0,0 +1,24 @@
+output "web_alb_sg_id" {
+  value = aws_security_group.web_alb_sg.id
+  description = "The ID of the web ALB security group"
+}
+
+output "web_ec2_sg_id" {
+  value = aws_security_group.web_ec2_sg.id
+  description = "The ID of the web EC2 security group"
+}
+
+output "app_alb_sg_id" {
+  value = aws_security_group.app_alb_sg.id
+  description = "The ID of the app ALB security group"
+}
+
+output "app_ec2_sg_id" {
+  value = aws_security_group.app_ec2_sg.id
+  description = "The ID of the app EC2 security group"
+}
+
+output "db_sg_id" {
+  value = aws_security_group.db_sg.id
+  description = "The ID of the db security group"
+}
